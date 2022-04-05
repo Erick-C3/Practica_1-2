c51bc21cef51ed46707b723c788ccc3e0b4303a9
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-05 11:53:42 -0300

    entrega de practica 1-1

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+console.log("funciona!");
+
+document.write(' <iframe width="560" height="315" src="https://www.youtube.com/embed/hw5xAUozcDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  ');
+
+document.write(' <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1952.6895882247557!2d-58.381638516929264!3d-34.60367221325362!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1648816940089!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>  ');
+
+document.write( '  <iframe src="https://cdn.pixabay.com/audio/2022/03/23/audio_07b2a04be3.mp3" frameborder="0"></iframe> ' );
+
+document.write( ' <iframe width="560" height="315"  src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>  ' );
+
+
+document.write( ' <iframe width="560" height="315"  src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>  '  );
